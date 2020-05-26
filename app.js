b09aad564093c92afbfff6e5137596ca26726dbc
saurabh_singh <imsaurabhsingh007@gmail.com>
2020-05-26 06:53:28 +0530

    Completed challenge 2

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-const fs = require('fs');
+const getNotes = require('./notes');
 
-fs.writeFileSync('notes.txt', 'This file was created by NodeJs');
-
-//append to file
-fs.appendFileSync('notes.txt', 'This text has been appended by node js');
+console.log(getNotes());
